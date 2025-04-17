32d4d9892e732e7de3c94ca302264c4afb69baba
irmu98 <choiroom98@naver.com>
2025-04-17 17:48:47 +0900

    fix: UseCase 추가로 Router 수정

--- lib/core/routing/router.dart
+++ lib/core/routing/router.dart
@@ -2,6 +2,7 @@ import 'package:go_router/go_router.dart';
 import 'package:search_app/core/routing/routes.dart';
 import 'package:search_app/data/data_source/photo_data_source_impl.dart';
 import 'package:search_app/data/repository/photo_repository_impl.dart';
+import 'package:search_app/domain/use_case/search_by_search_widget.dart';
 import 'package:search_app/presentation/search_detail/search_detail_screen.dart';
 import 'package:search_app/presentation/search_detail/search_detail_view_model.dart';
 import 'package:search_app/presentation/search_photo/search_photo_screen.dart';
@@ -12,29 +13,31 @@ final router = GoRouter(
   routes: [
     GoRoute(
       path: Routes.search,
-      builder:
-          (context, state) => SearchPhotoScreen(
-            viewModel: SearchPhotoViewModel(
-              photoRepository: PhotoRepositoryImpl(
-                photoDataSource: PhotoDataSourceImpl(),
-              ),
+      builder: (context, state) {
+        final photoRepository = PhotoRepositoryImpl(photoDataSource: PhotoDataSourceImpl());
+        return SearchPhotoScreen(
+          viewModel: SearchPhotoViewModel(
+            photoRepository: photoRepository,
+            searchBySearchWidget: SearchBySearchWidget(
+              photoRepository: photoRepository
             ),
           ),
+        );
+      },
     ),
     GoRoute(
       path: Routes.searchDetail,
-      builder:
-          (context, state) {
-            final photoId = int.parse(state.pathParameters['photoId'] ?? '-1');
-            return SearchDetailScreen(
-              viewModel: SearchDetailViewModel(
-                photoRepository: PhotoRepositoryImpl(
-                  photoDataSource: PhotoDataSourceImpl(),
-                ),
-                photoId: photoId,
-              ),
-            );
-          }
+      builder: (context, state) {
+        final photoId = int.parse(state.pathParameters['photoId'] ?? '-1');
+        return SearchDetailScreen(
+          viewModel: SearchDetailViewModel(
+            photoRepository: PhotoRepositoryImpl(
+              photoDataSource: PhotoDataSourceImpl(),
+            ),
+            photoId: photoId,
+          ),
+        );
+      },
     ),
   ],
 );
